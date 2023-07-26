45e7d72749b9ea0457e619352cea9e5d95f99563
钟志森 <625208758@qq.com>
2023-07-26 17:45:22 +0800

    feat: router的一些方法使用

--- src/views/testPage/RouterPage/RouterPage.vue
+++ src/views/testPage/RouterPage/RouterPage.vue
@@ -2,6 +2,8 @@
   <h1>router.push</h1>
   <el-button type="primary" @click="handleNavigateWithQuery">跳转about页面 - query传参</el-button>
   <el-button type="primary" @click="router.back()">返回上一页</el-button>
+  <el-button type="primary" @click="router.go(-2)">返回上两页</el-button>
+  <el-button type="primary" @click="router.replace('/about')">重定向</el-button>
 </template>
 
 <script setup lang="ts">
